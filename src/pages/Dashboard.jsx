4c25be2b2e3b21b28662d75a682a77c31df11f6c
Hasankhan170 <hasankhanali0316@gmail.com>
2024-09-28 13:57:10 +0500

    dashboard working

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,8 +1,44 @@
+import '../pages/Dashboard.css'
+
 
 const Dashboard = () => {
   return (
     <>
-    <h1>Dashboard</h1>
+    <>
+  {/* header section  */}
+  <header className="header-main">
+    <div className="blog-heading">
+    </div>
+  </header>
+  {/* dashboard section  */}
+  <h1 className="dashboard-head">Dashboard</h1>
+  {/* dashboard form  */}
+  <div className="dashboard-main">
+    <form className="dashboard-form">
+      <input
+        required=""
+        className="dashboard-input"
+        type="text"
+        placeholder="Enter Title.."
+      />
+      <br />
+      <textarea
+        required=""
+        className="dashboard-textarea"
+        placeholder="Text.."
+        rows=""
+        cols={180}
+        defaultValue={""}
+      />
+      <button className="publish-blog bg-warning">Publish Blog</button>
+    </form>
+  </div>
+  {/* my blogs section  */}
+  <h2 className="dashboard-head">My Blogs</h2>
+  {/* render all my blogs here  */}
+  <div className="my-blogs-render"></div>
+</>
+
     </>
   )
 }
