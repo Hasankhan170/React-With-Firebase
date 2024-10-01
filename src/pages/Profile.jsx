39b12f47dbeef19c53f9dfe62e3aa49cc18fcead
Hasankhan170 <hasankhanali0316@gmail.com>
2024-10-01 11:30:46 +0500

    working

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -30,20 +30,20 @@ function Profile() {
     <>
     <h1 className="m-5">Profile</h1>
     <div style={{margin: '0 auto',borderRadius:'10px', boxShadow: "rgba(0, 0, 0, 0.1) 0px 4px 12px",marginTop:'25px'}} className="container p-5 mt-40 max-h-max border-black">
-     <form style={{}} className="bg-warning">
-     <img width={150} className="mb-3" src={profileGet} alt="" />
+     <form style={{width:"200px",flexDirection:'column',border:'1px solid gray',borderRadius:'10px'}} className="flex text-center justify-center p-3">
+     <img width={200} style={{borderRadius:'10px'}} className="mb-3" src={profileGet} alt="" />
      <div>
-      {/* Hidden File Input */}
       <input type="file" id="fileInput" style={{ display: 'none' }} />
-
-      {/* Custom Button with Font Icon */}
-      <label htmlFor="fileInput" className="custom-file-upload">
+      <label htmlFor="fileInput" className="custom-file-upload ">
         <i className="fa fa-upload"></i> Upload File
       </label>
     </div>
-      <br />
-      <button type="submit" className="btn btn-warning">click</button>
+      <button type="submit" style={{width:'100%',textAlign:'start'}} className="btn btn-warning px-2 mt-2">Click</button>
      </form>
+
+
+
+     
       <h3 className="mb-3">hasan khan</h3>
       <p className="mb-3">password</p>
       <input  type="text" placeholder="Type here" className="input input-bordered input-outline-warning w-full mb-3" /><br />
