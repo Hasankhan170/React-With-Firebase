b57b4bd0d0f17762a65b88e5f29fdb23437e91d7
Hasankhan170 <hasankhanali0316@gmail.com>
2024-10-05 17:05:34 +0500

    working

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import { useEffect, useState } from "react";
-import { auth } from "./config/FirebaseConfig";
+import { auth, db } from "./config/FirebaseConfig";
+import { doc, getDoc } from "firebase/firestore"; // Import Firestore methods
 
 function App() {
   const [isUserLoggedIn, setIsUserLoggedIn] = useState(false);
   const [profileImage, setProfileImage] = useState(null);
 
   useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged((user) => {
+    const unsubscribe = auth.onAuthStateChanged( async(user) => {
       if (user) {
+        const userDoc = await getDoc(doc(db,'users',user.uid))
+        if(userDoc.exists()){
+          const userData = userDoc.data();
+          setProfileImage(userData.profileImage);
+        }
         setIsUserLoggedIn(true);
       } else {
         setIsUserLoggedIn(false);
